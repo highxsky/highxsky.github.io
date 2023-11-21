09c030a7455d9bcb70e3c8fbc9ab074dc40ad2f9
Pierre Marionneau <marionneaupierre@live.fr>
2023-11-20 22:56:49 -0800

    new release of my website

--- javascript/javascript.js
+++ javascript/javascript.js
@@ -1,10 +1,10 @@
 function myFunction() {
-  var navigationMenu = document.getElementById("navigation_menu");
+  var navigationMenu = document.getElementById("navigation-menu");
   navigationMenu.classList.toggle("show");
 
   var content = document.getElementById("content"); // Assuming "content" is the content below the menu
   content.style.marginTop = navigationMenu.classList.contains("show") ? navigationMenu.clientHeight + "px" : "0";
 
-  var navigationButton = document.querySelector(".navigation_button");
+  var navigationButton = document.querySelector(".navigation-button");
   navigationButton.classList.toggle("rotate");
 }
